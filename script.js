547d14a2d0115460581991dc72aa5308bec7b73e
Ben Weeks <ben.weeks130@gmail.com>
2025-06-22 23:15:58 -0400

    resizing is broken and gradual opacity needs to be fixed

--- script.js
+++ script.js
@@ -1,34 +1,57 @@
 // declare some constants
-const cellNum = 16;
+let cellNum = 32;
 
 const board = document.querySelector(".board");
 const boardWidth = parseInt(window.getComputedStyle(board).width);
 
-const cellSize = Math.floor(boardWidth / cellNum);
+const cellSize = boardWidth / cellNum;
 console.log(cellSize);
 
 // board creation
-for (let i = 0; i < (cellNum * cellNum); i++) {
-    const cell = document.createElement("div");
-    cell.classList.add("cell");
-    cell.style.width = `${cellSize}px`;
-    cell.style.height = `${cellSize}px`;
+function constructBoard() {
+    for (let i = 0; i < (cellNum * cellNum); i++) {
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cell.style.width = `${cellSize}px`;
+        cell.style.height = `${cellSize}px`;
 
-    board.appendChild(cell);
-}
+        board.appendChild(cell);
+    }
+};
+
+constructBoard();
 
 // draw on board
 const cells = document.querySelectorAll(".cell");
 cells.forEach((cell) => {
     cell.addEventListener('mouseenter', () => {
-        cell.style.backgroundColor = "black";
+        let op = cell.style.opacity;
+        cell.style.opacity = op + 0.1;
+        console.log(cell.style.opacity);
     });
 });
 
-// clear board
-const clearButton = document.querySelector(".clear");
-clearButton.addEventListener('click', () => {
+// clear board function
+function clearBoard() {
     cells.forEach((cell) => {
-        cell.style.backgroundColor = "white";
+        cell.style.opacity = 0;
     });
+};
+
+const clearButton = document.querySelector(".clear");
+clearButton.addEventListener('click', clearBoard);
+
+// resize board 
+const resizeButton = document.querySelector(".resize");
+resizeButton.addEventListener('click', () => {
+    // prompt user
+    cellNum = parseInt(prompt("Enter a number between 8-64: ", "32"));
+    // bounds
+    cellNum = (cellNum > 64) ? 64 : cellNum;
+    cellNum = (cellNum < 8) ? 8 : cellNum;
+
+    cells.forEach((cell) => { board.removeChild(cell); })
+
+    constructBoard();
+    clearBoard();
 });
